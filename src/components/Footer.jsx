716d2fff5499163d4dcad67a7c31955059877ce1
Hanif Robby <96159381+HanifRobby@users.noreply.github.com>
2023-11-16 02:34:31 +0700

    Update Page Navbar,Footer : changed anchor to navlink

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,44 +1,51 @@
-
+import { NavLink } from "react-router-dom";
+import { horizontalLogo } from "../assets";
 
 const Footer = () => {
   return (
-    <div className='flex flex-col gap-7'>
-        <div className='flex flex-row justify-between'>
-            <div className='flex flex-col gap-4'>
-                <img src='' className='w-80 h-24'/>
-                <p className='line-clamp-2 text-secondary'>we transform your unwanted items into cash effortlessly.</p>
-            </div>
-            <div className='flex flex-row gap-28'>
-                <div className='flex flex-col'>
-                    <a href="">Navigation</a>
-                    <a href="">Home</a>
-                    <a href="">About us</a>
-                    <a href="">FAQ</a>
-                </div>
-                <div className='flex flex-col'>
-                    <a href="">Support Us</a>
-                    <a href="">Contact Us</a>
-                    <a href="">Support Center</a>
-                    <a href="">Privacy and Policy</a>
-                </div>
-                <div className='flex flex-col'>
-                    <a href="">Partner</a>
-                    <a href="">Our Partner</a>
-                    <a href="">Community</a>
-                    <a href="">Investor</a>
-                </div>
+    <div className="flex flex-col">
+    
+      {/* line */}
+      <div className="w-full h-0.5 bg-black" />
 
-            </div>
+      <div className="flex flex-row justify-between p-9">
+        <div className="flex flex-col gap-4">
+          <img src={horizontalLogo} className="w-80 h-24" />
+          <p className="text-secondary text-2xl w-80">
+            we transform your unwanted items into cash effortlessly.
+          </p>
         </div>
-        <div className='flex flex-row justify-between'>
-            <p className='text-secondary'>© 2023 HandMeDown, All Rights Reserved</p>
-            <div>
-                {/* 3 socials */}
-            </div>
+        <div className="flex flex-row gap-28 text-2xl">
+          <div className="flex flex-col gap-2">
+            <NavLink to="">Navigation</NavLink>
+            <NavLink to="">Home</NavLink>
+            <NavLink to="">About us</NavLink>
+            <NavLink to="">FAQ</NavLink>
+          </div>
+          <div className="flex flex-col gap-2">
+            <NavLink to="">Support Us</NavLink>
+            <NavLink to="">Contact Us</NavLink>
+            <NavLink to="">Support Center</NavLink>
+            <NavLink to="">Privacy and Policy</NavLink>
+          </div>
+          <div className="flex flex-col gap-2">
+            <NavLink to="">Partner</NavLink>
+            <NavLink to="">Our Partner</NavLink>
+            <NavLink to="">Community</NavLink>
+            <NavLink to="">Investor</NavLink>
+          </div>
         </div>
+      </div>
 
+      {/* line */}
+      <div className="w-full h-0.5 bg-black" />
+
+      <div className="flex flex-row justify-between p-9 text-2xl">
+        <p className="text-secondary">© 2023 HandMeDown, All Rights Reserved</p>
+        <div>{/* 3 socials */}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
