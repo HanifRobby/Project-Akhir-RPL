6d52096af72cd2914b01f9d81a8b1d454d9d0610
HanifRobby <96159381+HanifRobby@users.noreply.github.com>
2023-11-15 22:26:15 +0700

    update home

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,11 @@
 import horizontalLogo from './horizontalLogo.png'
 import background1 from './background1.png'
+import landing1 from './landing1.jpg'
+import landing2 from './landing2.png'
 
 export {
     horizontalLogo,
-    background1
+    background1,
+    landing1,
+    landing2
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,30 @@
-
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+import { horizontalLogo } from "../assets";
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row h-44 justify-between items-center bg-primary p-11">
-      <img src="" className="w-80 h-24" />
-      <div className="flex flex-row gap-20 justify-between">
+    <div className="flex flex-row h-20 justify-between items-center bg-primary p-11">
+      <img src={horizontalLogo} className="w-72 h-20" />
+      <div className="flex flex-row gap-20 justify-between items-center">
         <div className="flex flex-row gap-12 justify-between">
-          <a href="" className="font-jua text-xl">
+          <NavLink to="" className="font-jua text-xl">
             Home
-          </a>
-          <a href="" className="font-jua text-xl">
+          </NavLink>
+          <NavLink href="" className="font-jua text-xl">
             Products
-          </a>
-          <a href="" className="font-jua text-xl">
+          </NavLink>
+          <NavLink href="" className="font-jua text-xl">
             Resources
-          </a>
+          </NavLink>
+        </div>
+        <div className="flex flex-row justify-between items-center gap-6">
+          <NavLink href="" className="bg-secondary text-white px-6 py-2 rounded-md">
+            Login
+          </NavLink>
+          <NavLink href="" className="bg-secondary text-white px-6 py-2 rounded-md">
+            Register
+          </NavLink>
         </div>
       </div>
     </div>
