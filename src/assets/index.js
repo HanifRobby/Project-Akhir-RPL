c5648935590a0f0fb39a9ff94ca779f0faaf577f
Hanif Robby <96159381+HanifRobby@users.noreply.github.com>
2023-11-16 02:01:43 +0700

    adding image for register page

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,24 @@ import horizontalLogo from './horizontalLogo.png'
 import background1 from './background1.png'
 import landing1 from './landing1.jpg'
 import landing2 from './landing2.png'
+import register1 from './register1.png'
+import usernameImage from './username.png'
+import emailImage from './email.png'
+import phoneImage from './phone.png'
+import passwordImage from './password.png'
+import confirmImage from './confirm.png'
+import eye from './eye.png'
 
 export {
     horizontalLogo,
     background1,
     landing1,
-    landing2
+    landing2,
+    register1,
+    usernameImage,
+    emailImage,
+    phoneImage,
+    passwordImage,
+    confirmImage,
+    eye,
 }
