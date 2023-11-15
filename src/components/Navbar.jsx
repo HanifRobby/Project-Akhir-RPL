ccc0f3f6c96e6d9ee101e80e5126fc164bf8d498
Hanif Robby <96159381+HanifRobby@users.noreply.github.com>
2023-11-15 22:32:25 +0700

    navbar homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-
+import {horizontalLogo} from '../assets'
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row h-44 justify-between items-center bg-primary p-11">
-      <img src="" className="w-80 h-24" />
+    <div className="flex flex-row h-44 justify-between items-center bg-primary p-11 border-b-4">
+      <img src={horizontalLogo} className="w-80 h-24" />
       <div className="flex flex-row gap-20 justify-between">
-        <div className="flex flex-row gap-12 justify-between">
+        <div className="flex flex-row gap-12 justify-between items-center">
           <a href="" className="font-jua text-xl">
             Home
           </a>
@@ -15,6 +15,10 @@ const Navbar = () => {
           <a href="" className="font-jua text-xl">
             Resources
           </a>
+        </div>
+        <div className='flex flex-row gap-11 justify-between items-center'>
+          <a href='' className='bg-secondary text-white px-10 py-3 rounded-lg'>Login</a>
+          <a href='' className='bg-secondary text-white px-10 py-3 rounded-lg'>Register</a>
         </div>
       </div>
     </div>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,9 +7,10 @@ const Homepage = () => {
       <Navbar />
 
       {/* Body */}
-      <div className="flex flex-col bg-secondary justify-center items-center py-20 px-11 gap-10">
-        <h1 className="text-white text-8xl">HandMeDown</h1>
-        <p className="text-white text-2xl">Discover the easiest way to sell your pre-loved items with our professional and user-friendly second-hand marketplace website services! At our platform, we transform your unwanted items into cash effortlessly.</p>
+      <div className="flex flex-col bg-primary justify-center items-center py-20 px-11 gap-10">
+        <h1 className="text-secondary text-8xl">HandMeDown</h1>
+        <p className="text-secondary text-2xl">Discover the easiest way to sell your pre-loved items with our professional and user-friendly second-hand marketplace website services! At our platform, we transform your unwanted items into cash effortlessly.</p>
+        <a href="" className="flex flex-row items-center justify-center gap-4 text-xl text-white bg-secondary px-8 py-4 rounded-2xl"><img src="" className="w-14 h-14"/>Get Started</a>
         <img src="" className="w-11/12 h-[768px]"/>
       </div>
       <div className="flex flex-row bg-white justify-center items-center py-20 px-11">
